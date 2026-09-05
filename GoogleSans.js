e2df33789bf060f16cccb705b76e2802548e2bd6
iamhaunt <162820354+iamhaunt@users.noreply.github.com>
2026-09-06 02:17:23 +0700

    Update GoogleSans.js

--- GoogleSans.js
+++ GoogleSans.js
@@ -1,23 +1,23 @@
 // ==UserScript==
-// @name        Universal Google Sans
+// @name        Universal Google Sans 600
 // @namespace   Violentmonkey Script
 // @description Apply Google Sans Universally
-// @version     v1.0
+// @version     v1.2
 // @match       *://*/*
 // @author      iamhaunt
 // @license     MIT
 // @grant       GM_addStyle
 // @grant       GM_getResourceURL
-// @resource    myFont https://raw.githubusercontent.com/iamhaunt/greasy-fork-GoogleSans/main/GoogleSans-Regular.woff2
+// @resource    myFont https://raw.githubusercontent.com/iamhaunt/greasy-fork-GoogleSans/main/GoogleSans-Regular%20600.woff2
 // @run-at      document-start
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+ 
     const fontUrl = GM_getResourceURL('myFont');
     const fontName = 'CustomGoogleSans';
-
+ 
     const css = `
         @font-face {
             font-family: '${fontName}';
@@ -28,9 +28,9 @@
             font-family: '${fontName}', sans-serif !important;
         }
     `;
-
+ 
     GM_addStyle(css);
-
+ 
     const inject = (t) => {
         if (!t || t.querySelector(`style[data-f="${fontName}"]`)) return;
         const s = document.createElement('style');
@@ -38,7 +38,7 @@
         s.textContent = css;
         t.appendChild(s);
     };
-
+ 
     const process = (n) => {
         if (n.nodeType !== 1) return;
         if (n.shadowRoot) inject(n.shadowRoot);
@@ -47,13 +47,13 @@
             if (children[i].shadowRoot) inject(children[i].shadowRoot);
         }
     };
-
+ 
     new MutationObserver(ms => {
         for (let i = 0; i < ms.length; i++) {
             const added = ms[i].addedNodes;
             for (let j = 0; j < added.length; j++) process(added[j]);
         }
     }).observe(document.documentElement, { childList: true, subtree: true });
-
+ 
     process(document.documentElement);
 })();
